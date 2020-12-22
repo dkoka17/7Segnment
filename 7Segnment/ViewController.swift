d0027afe4b6d350c50096eaf79a9026daee53e23
dkoka17 <dkoka17@freeuni.edu.ge>
2020-12-21 22:12:23 -0800

    Initial Commit

---
+++ 7Segnment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7Segnment
+//
+//  Created by dato on 12/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
